2c12d8b7197d77290a14b5cfc908ff1a82b3f529
Anushimpi1 <anushkashimpi10@gmail.com>
2026-04-07 17:04:29 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -6,3 +6,12 @@ app = main_app
 @app.get("/")
 def home():
     return {"message": "Your app is working"}
+
+
+def main():
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
