3e9949c18d0857babee376af11643b9b3236aa2c
Anushimpi1 <anushkashimpi10@gmail.com>
2026-04-12 14:23:10 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,5 +1,17 @@
+from fastapi import FastAPI
+from server import app as main_app
 
-from server import app, main 
+app = main_app
+
+@app.get("/")
+def home():
+    return {"message": "Your app is working"}
+
+
+def main():
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=7860)
+
 
 if __name__ == "__main__":
     main()
